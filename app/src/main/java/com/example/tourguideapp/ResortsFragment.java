d60cae026e2af2d0c0913509a1d0b70adf242c20
Anthony <tonyudeagbala@gmail.com>
2022-08-26 14:56:24 +0100

    Create ResortsFragment

---
+++ app/src/main/java/com/example/tourguideapp/ResortsFragment.java
@@ -0,0 +1,18 @@
+package com.example.tourguideapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ResortsFragment extends Fragment {
+
+
+    public ResortsFragment() {
+        // Required empty public constructor
+    }
+
+}
