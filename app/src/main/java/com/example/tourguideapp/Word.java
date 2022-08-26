f898f55c80377be4e7b374800fb2f2119f293e3a
Anthony <tonyudeagbala@gmail.com>
2022-08-26 14:09:43 +0100

    Change String data types to int

--- app/src/main/java/com/example/tourguideapp/Word.java
+++ app/src/main/java/com/example/tourguideapp/Word.java
@@ -2,21 +2,21 @@ package com.example.tourguideapp;
 
 public class Word {
     /** Title of the tourist center */
-    private String mTitle;
+    private int mTitle;
 
     /** Description of the tourist center */
-    private String mDescription;
+    private int mDescription;
 
     /** Description of the tourist center */
-    private String mAddress;
+    private int mAddress;
 
     /** Image resource Id for the word */
-    private int mImageResourceId = 0;
+    private int mImageResourceId;
 
     /** states if an image is present or not*/
-//    private static int NO_IMAGE_PROVIDED = -1;
+    private static int NO_IMAGE_PROVIDED = -1;
 //
-    public  Word(String title, String description, String address, int imageResourceId) {
+    public  Word(int title, int description, int address, int imageResourceId) {
         mTitle = title;
         mAddress = address;
         mDescription = description;
@@ -24,17 +24,17 @@ public class Word {
     }
 
     /** Get the title of the tourist center*/
-    public String getTitle() {
+    public int getTitle() {
         return mTitle;
     }
 
     /** Get the description of the tourist center*/
-    public String getDesription() {
+    public int getDesription() {
         return mDescription;
     }
 
     /** Get the address of the tourist center*/
-    public String getAddress() {
+    public int getAddress() {
         return mAddress;
     }
 
