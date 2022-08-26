1cdfa111c6bd08032e964787ffb4b30f60e4f7a6
Anthony <tonyudeagbala@gmail.com>
2022-08-26 17:55:28 +0100

    Add words to fragments

--- app/src/main/java/com/example/tourguideapp/ResortsFragment.java
+++ app/src/main/java/com/example/tourguideapp/ResortsFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ResortsFragment extends Fragment {
 
@@ -15,4 +18,86 @@ public class ResortsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        final ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word(
+                R.string.juanita_hotels_title,
+                R.string.juanita_hotels_description,
+                R.string.juanita_hotels_address,
+                R.drawable.juanita_hotels
+        ));
+        words.add(new Word(
+                R.string.landmark_hotel_title,
+                R.string.landmark_hotel_description,
+                R.string.landmark_hotel_address,
+                R.drawable.landmark_hotel
+        ));
+        words.add(new Word(
+                R.string.visa_karena_hotels_title,
+                R.string.visa_karena_hotels_description,
+                R.string.visa_karena_hotels_address,
+                R.drawable.visa_karena_hotels
+        ));
+        words.add(new Word(
+                R.string.best_premier_hotel_title,
+                R.string.best_premier_hotel_description,
+                R.string.best_premier_hotel_address,
+                R.drawable.best_premier_hotel
+        ));
+        words.add(new Word(
+                R.string.golden_tulip_hotel_title,
+                R.string.golden_tulip_hotel_description,
+                R.string.golden_tulip_hotel_address,
+                R.drawable.golden_tulip_hotel
+        ));
+        words.add(new Word(
+                R.string.labake_cottage_hotel_title,
+                R.string.labake_cottage_hotel_description,
+                R.string.labake_cottage_hotel_address,
+                R.drawable.labake_ottage_hotel
+        ));
+        words.add(new Word(
+                R.string.bougainvillea_hotels_title,
+                R.string.bougainvillea_hotels_description,
+                R.string.bougainvillea_hotels_address,
+                R.drawable.bougainvillea_hotels
+        ));
+        words.add(new Word(
+                R.string.the_crib_lifestyle_hotel_title,
+                R.string.the_crib_lifestyle_hotel_description,
+                R.string.the_crib_lifestyle_hotel_address,
+                R.drawable.the_crib_lifestyle_hotel
+        ));
+        words.add(new Word(
+                R.string.christine_apartment_hotel_ltd_title,
+                R.string.christine_apartment_hotel_ltd_description,
+                R.string.christine_apartment_hotel_ltd_address,
+                R.drawable.christine_apartment_hotel_ltd
+        ));
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        WordAdapter adapter = new WordAdapter(getActivity(), words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
 }
--- app/src/main/java/com/example/tourguideapp/RestaurantsFragment.java
+++ app/src/main/java/com/example/tourguideapp/RestaurantsFragment.java
@@ -7,58 +7,116 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RestaurantsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class RestaurantsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public RestaurantsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RestaurantsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RestaurantsFragment newInstance(String param1, String param2) {
-        RestaurantsFragment fragment = new RestaurantsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurants, container, false);
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        final ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word(
+                R.string.piazza_restaurant_title,
+                R.string.race_and_equity_event_description,
+                R.string.piazza_restaurant_address,
+                R.drawable.piazza_restaurant
+        ));
+        words.add(new Word(
+                R.string.south_66_restaurant_title,
+                R.string.south_66_restaurant_description,
+                R.string.south_66_restaurant_address,
+                R.drawable.south_66_restaurant
+        ));
+        words.add(new Word(
+                R.string.asia_town_restaurant_title,
+                R.string.asia_town_restaurant_description,
+                R.string.asia_town_restaurant_address,
+                R.drawable.asia_town_restaurant
+        ));
+        words.add(new Word(
+                R.string.yogurberry_restaurant_title,
+                R.string.yogurberry_restaurant_description,
+                R.string.yogurberry_restaurant_address,
+                R.drawable.yogurberry_restaurant
+        ));
+        words.add(new Word(
+                R.string.spice_route_restaurant_title,
+                R.string.spice_route_restaurant_description,
+                R.string.spice_route_restaurant_address,
+                R.drawable.spice_route_restaurant
+        ));
+        words.add(new Word(
+                R.string.woodhouse_cafe_restaurant_title,
+                R.string.woodhouse_cafe_restaurant_description,
+                R.string.woodhouse_cafe_restaurant_address,
+                R.drawable.woodhouse_cafe_restaurant
+        ));
+        words.add(new Word(
+                R.string.buns_and_batter_restaurant_title,
+                R.string.buns_and_batter_restaurant_description,
+                R.string.buns_and_batter_restaurant_address,
+                R.drawable.buns_and_batter_restaurant
+        ));
+        words.add(new Word(
+                R.string.genesis_sky_bar_restaurant_title,
+                R.string.genesis_sky_bar_restaurant_description,
+                R.string.genesis_sky_bar_restaurant_address,
+                R.drawable.genesis_sky_bar_restaurant
+        ));
+        words.add(new Word(
+                R.string.the_coffee_shop_restaurant_title,
+                R.string.the_coffee_shop_restaurant_description,
+                R.string.the_coffee_shop_restaurant_address,
+                R.drawable.the_coffee_shop_restaurant
+        ));
+        words.add(new Word(
+                R.string.the_wok_chinese_restaurant_title,
+                R.string.the_wok_chinese_restaurant_description,
+                R.string.the_wok_chinese_restaurant_address,
+                R.drawable.the_wok_chinese_restaurant
+        ));
+        words.add(new Word(
+                R.string.cornerstore_cafe_restaurant_title,
+                R.string.cornerstore_cafe_restaurant_description,
+                R.string.cornerstore_cafe_restaurant_address,
+                R.drawable.cornerstore_cafe_restaurant
+        ));
+        words.add(new Word(
+                R.string.crinkles_n_cream_restaurant_title,
+                R.string.crinkles_n_cream_restaurant_description,
+                R.string.crinkles_n_cream_restaurant_address,
+                R.drawable.crinkles_n_cream_restaurant
+        ));
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        WordAdapter adapter = new WordAdapter(getActivity(), words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
+
 }
